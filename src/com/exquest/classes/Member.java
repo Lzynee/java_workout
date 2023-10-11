dd28485613c7208a0822ed9f9ba70c5a2c40cb66
Lzyne <raina2zen90@gmail.com>
2023-10-11 20:16:22 +0900

    필드 및 생성자 선언, 메소드 호출 연습

---
+++ src/com/exquest/classes/Member.java
@@ -0,0 +1,15 @@
+package com.exquest.classes;
+
+import javax.lang.model.element.Name;
+
+public class Member {
+    // 필드 선언
+    String name;
+    String id;
+    String password;
+    int age;
+
+    // 생성자 선언
+    Member(String name, String id) {
+    }
+}
