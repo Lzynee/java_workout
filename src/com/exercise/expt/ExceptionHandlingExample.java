5f2678554d35390ec9395f47d74cb777ab073dc6
Lzyne <raina2zen90@gmail.com>
2023-10-13 16:57:50 +0900

    예외 처리 코드 - 예외 종류에 따라 처리하기 연습

---
+++ src/com/exercise/expt/ExceptionHandlingExample.java
@@ -0,0 +1,21 @@
+package com.exercise.expt;
+
+public class ExceptionHandlingExample {
+    public static void main(String[] args) {
+        String[] array = {"100", "1oo", null, "200"};
+
+        for (int i=0; i<= array.length; i++) {
+            try {
+                int value = Integer.parseInt(array[i]);
+                System.out.printf("array[%d]: %d%n", i, value);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("배열 인덱스가 초과됨: " + e.getMessage());
+            } catch (NullPointerException | NumberFormatException e) {
+                System.out.println("데이터에 문제가 있음: " + e.getMessage());
+            } catch (Exception e){
+//                System.out.println("실행에 문제가 있습니다.");
+            }
+            // try ~ catch
+        }  // for
+    }  // main
+}  // class
